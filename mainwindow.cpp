7e6ff375b69d60588433e785fc22898412840355
Matt Shindala <matt.shindala@gmail.com>
2016-09-09 23:04:48 -0400

    add slider at bottom

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2,15 +2,36 @@
 #include "song.h"
 #include <iostream>
 
+// how many "steps" the slider has; e.g. it ranges from 0 to this number
+#define SLIDER_RESOLUTION 10000
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
+    layout = new QVBoxLayout(this);
     treeView = new QTreeView(this);
+    layout->addWidget(treeView);
+    slider = new QSlider(Qt::Horizontal, this);
+    slider->setDisabled(true);
+    slider->setRange(0, SLIDER_RESOLUTION);
+    layout->addWidget(slider);
+
+    centralWidget = new QWidget(this);
+    centralWidget->setLayout(layout);
+    setCentralWidget(centralWidget);
+
     model = new MusicModel(treeView);
-    player = new QMediaPlayer(this);
     treeView->setModel(model);
-    setCentralWidget(treeView);
+
+    player = new QMediaPlayer(this);
+    // a lower interval uses more CPU, but makes the slider smoother. 
+    player->setNotifyInterval(10);
+
     connect(treeView, SIGNAL(doubleClicked(QModelIndex)), this, SLOT(playSong(QModelIndex)));
+    connect(player, SIGNAL(positionChanged(qint64)), this, SLOT(songPositionChanged(qint64)));
+    connect(player, SIGNAL(stateChanged(QMediaPlayer::State)), this, SLOT(playerStateChanged(QMediaPlayer::State)));
+    connect(slider, SIGNAL(sliderMoved(int)), this, SLOT(sliderPositionChanged(int)));
+
     this->setMinimumHeight(600);
     this->setMinimumWidth(800);
 }
@@ -28,4 +49,32 @@ void MainWindow::playSong(QModelIndex index)
     }
     player->setMedia(QUrl::fromLocalFile(s->path));
     player->play();
+}
+
+void MainWindow::songPositionChanged(qint64 position)
+{
+    // don't change the position of the slider while the user is adjusting it
+    if (!slider->isSliderDown()) {
+      float relativePosition = ((float) position / (float) player->duration()) * SLIDER_RESOLUTION;
+      slider->setValue(relativePosition);
+    }
+}
+
+void MainWindow::sliderPositionChanged(int newValue)
+{
+  qint64 relativePosition = ((float) newValue / SLIDER_RESOLUTION) * (float) player->duration();
+  player->setPosition(relativePosition);
+}
+
+void MainWindow::playerStateChanged(QMediaPlayer::State state)
+{
+  switch (state) {
+    case QMediaPlayer::StoppedState:
+      slider->setDisabled(true);
+      break;
+    case QMediaPlayer::PlayingState:
+    case QMediaPlayer::PausedState:
+    default:
+      slider->setDisabled(false);
+  }
 }
--- mainwindow.h
+++ mainwindow.h
@@ -5,6 +5,9 @@
 #include <QTreeView>
 #include <QObject>
 #include <QMediaPlayer>
+#include <QSlider>
+#include <QVBoxLayout>
+#include <QWidget>
 #include "musicmodel.h"
 
 class MainWindow : public QMainWindow
@@ -15,11 +18,17 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 private:
+    QWidget* centralWidget;
     QTreeView* treeView;
     MusicModel* model;
     QMediaPlayer* player;
+    QSlider* slider;
+    QVBoxLayout* layout;
 private slots:
     void playSong(QModelIndex index);
+    void songPositionChanged(qint64 position);
+    void sliderPositionChanged(int newValue);
+    void playerStateChanged(QMediaPlayer::State state);
 };
 
 #endif // MAINWINDOW_H
